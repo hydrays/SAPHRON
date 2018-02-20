d26fd16adcbd212774ee9d8597bbc3bff6d37a68
hydra <hydrays@gmail.com>
2018-02-20 22:01:55 +0800

    Langevin version for the LebwohlLasher potential.

--- src/ForceFields/LebwohlLasherFF.h
+++ src/ForceFields/LebwohlLasherFF.h
@@ -28,8 +28,8 @@ namespace SAPHRON
 
 			double dot = fdot(n1,n2);
 
-			// return {-1.0*(_eps*(1.5*dot*dot - 0.5) + _gamma), 0.0};
-			return {-1.0*(_eps*(35*dot*dot - 30*dot*dot + 15/8) + _gamma), 0.0};
+			return {-1.0*(_eps*(1.5*dot*dot - 0.5) + _gamma), 0.0};
+			//return {-1.0*(_eps*(35*dot*dot - 30*dot*dot + 15/8) + _gamma), 0.0};
 		}
 
 		// Serialize LJ.
@@ -40,12 +40,25 @@ namespace SAPHRON
 			json["epsilon"] = _eps;
 		}
 
-		virtual double EvaluateTorque(const Particle&, 
-									 const Particle&, 
-									 const Position& rij,
-									 unsigned int wid) const override
+		virtual double EvaluateTorque(const Particle& p1,
+									 const Particle& p2, 
+									 const Position&,
+									 unsigned int) const override
 		{
-			return 0.0;
+			double torque = 0.0;
+			
+			auto& ui = p1.GetDirector();
+			auto& uj = p2.GetDirector();
+
+			auto uiuj = fdot(ui, uj);
+			auto dUdg = 2.0*uiuj;
+			auto uxu = ui[0]*uj[1] - ui[1]*uj[0];
+			torque = 1.5*_eps*dUdg * uxu;
+			//printf("inside GayBerne->Evaluate: u1 = (%f, %f), u2 = (%f, %f), r = (%f, %f), R=%f, torque=%f\n", 
+			//	ui[0], ui[1], uj[0], uj[1], rij[0], rij[1], R, torque);
+			//printf("%f, %f, %f, %f, %f, %f, %f, %f\n", 
+			//	ui[0], ui[1], uj[0], uj[1], rij[0], rij[1], R, torque);
+			return torque;
 		}
 
 		double GetEpsilon() { return _eps; }
