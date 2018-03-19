df5c493f6362de25ea29566ff69cf728c0db5156
hydrays <huyc@tsinghua.edu.cn>
2018-03-19 11:24:23 +0800

    prepare to make a server run

--- project/StochasticResonance/postprocess.r
+++ project/StochasticResonance/postprocess.r
@@ -1,58 +1,116 @@
+require(jsonlite)
 ##require("pracma")
-##require(lattice)
+require(lattice)
 
-sys <- "sys1_mc"
+col.l <- colorRampPalette(c('red', 'orange', 'yellow', 'green', 'cyan', 'blue'))
+
+sys <- "sys_n_2500_beta_0.5"
+##sys <- "sys_n_625_beta_0.5"
 
 OP <- function(u, v)
 {
     ##OP <- mean(cos(2*angles))
-    OP <- mean(2*(u/sqrt(u*u + v*v))*(u/sqrt(u*u + v*v)) - 1)
+    ##OP <- mean(2*(u/sqrt(u*u + v*v))*(u/sqrt(u*u + v*v)) - 1)
+    OP <- mean(((u+v)/sqrt(u*u + v*v))**2 - 1)
     return(OP)
 }
 
-id <- 0
-QList <- NULL
-for ( rep in seq(40) )
+cc <- 0.5
+xmax <- 25.5
+
+outdata <- NULL
+case_id <- 0
+for ( rep2 in seq(19) )
 {
-    id = id + 1
-    cat("\n ***********run********* ", id, "\n")
-    out <- paste(sys, '_', id, '.json', sep='')
-    FolderName <- paste("case_", sys, "_", as.character(id), sep='')
-
-    AA <- read.table(paste(FolderName, '/out.sys1.world0.particle.dat', sep=''), header=T)
-    A <- subset(AA, Iteration==0)
-    NL <- dim(AA)[1]
-    N <- dim(A)[1]
-    L <- NL/N
-    T <- 2*1000
-
-    SList <- NULL
-    Vsin <- NULL
-    Vcos <- NULL
-    figure_index <- 0
-    for ( i in seq(L) )
+    case_id <- case_id + 1
+    
+    id <- 0
+    QList <- NULL
+    DList <- NULL
+    for ( rep in seq(21) )
     {
-        figure_index <- figure_index + 1
-        iter <- AA[N*i, 1]
-        A <- subset(AA, Iteration==iter)
-        ## padded_figure_index <- sprintf("%05d", figure_index)
-        ## png(paste("config_", padded_figure_index, ".png", sep=''))
-        ## plot(A$x, A$y, main=iter, cex=0.5, xlab="x", ylab="y")
-        ## arrows(A$x-.25*A$ux, A$y-.25*A$uy, A$x+.25*A$ux, A$y+.25*A$uy, length=0.0)
+        id = id + 1
+        cat("\n ***********run********* ", id, "\n")
+        out <- paste(sys, '_', id, '.json', sep='')
+        FolderName <- paste("case_", cc, "_", sys, "_", as.character(case_id), "_", as.character(id), sep='')
+
+        AA <- read.table(paste(FolderName, '/out.sys1.world0.particle.dat', sep=''), header=T)
+        A <- subset(AA, Iteration==0)
+        NL <- dim(AA)[1]
+        N <- dim(A)[1]
+        L <- NL/N
+        T <- 50000
+
+        SList <- NULL
+        Vsin <- NULL
+        Vcos <- NULL
+        figure_index <- 0
+        for ( i in seq(L) )
+        {
+            figure_index <- figure_index + 1
+            iter <- AA[N*i, 1]
+            A <- subset(AA, Iteration==iter)
+
+            ## padded_figure_index <- sprintf("%05d", figure_index)
+            ## png(paste("config_", padded_figure_index, ".png", sep=''))
+            ## plot(A$x, A$y, main=iter, cex=0.5, xlab="x", ylab="y")
+            ## arrows(A$x-.25*A$ux, A$y-.25*A$uy, A$x+.25*A$ux, A$y+.25*A$uy, length=0.0)
+            ## dev.off()
+            ## S <- OP(A$ux, A$uy)
+            
+            P <- rbind(subset(A, A$x == 0.5 & A$y == 0.5), subset(A, A$x == 0.5 & A$y == xmax-0.5 ),
+                       subset(A, A$x == xmax-0.5 & A$y == 0.5), subset(A, A$x == xmax-0.5 & A$y == xmax-0.5 ))
+            ##P <- rbind(subset(A, A$x == 0.5 & A$y == 0.5), subset(A, A$x == xmax - 0.5 & A$y == xmax - 0.5 ))
+            ##P <- A[c(25, 597), ]
+            S <- OP(P$ux, P$uy)
+            
+            SList <- c(SList, S)
+            Vsin <- c(Vsin, sin(2.0*pi*iter/T))
+            Vcos <- c(Vcos, cos(2.0*pi*iter/T))
+        }
+        js <- read_json(paste(FolderName, '/out.sys1.chkpt.json', sep=''))
+        D <- js$worlds[[1]]$temperature
+        amp <- js$forcefields$A
+        Tn <- js$forcefields$T
+        In <- js$observers[[1]]$frequency
+        Tt <- js$iterations
+
+        Q <- In*2.0*sqrt( (SList %*% Vsin)**2 + (SList %*% Vcos)**2 )/Tt
+
+        if (id == 1)
+        {
+            dQ <- 1
+            Q0 <- Q
+        }
+        else
+        {
+            dQ <- Q/Q0
+        }
+        
+        cat(amp, D, Q, dQ, '\n')
+        QList <- c(QList, Q)
+        DList <- c(DList, D)
+
+        outdata <- rbind(outdata, c(amp, D, Q, dQ))
+
+        ## pdf(paste("op/op_", FolderName, ".pdf", sep=''), height=4, width=9)
+        ## par(mar=c(4,6,4,4))
+        ## plot(Vsin[1:100], type='l', xlab=expression(t), ylab=expression(S), cex.lab=1.5, cex.axis=1.5)
+        ## lines(SList[1:100], type='o', col='blue', lwd=2, cex=0.75)
         ## dev.off()
-        S <- OP(A$ux, A$uy)
-        SList <- c(SList, S)
-        Vsin <- c(Vsin, sin(2.0*pi*iter/T))
-        Vcos <- c(Vcos, cos(2.0*pi*iter/T))
+
     }
-    Q <- sqrt( (SList %*% Vsin)**2 + (SList %*% Vcos)**2 ) 
-    cat(Q, '\n')
-    QList <- c(QList, Q)
+
+    ## png("QvsD.png")
+    ## plot(QList, type='o')
+    ## dev.off()
+
+    ## write.csv(cbind(QList, DList), file=paste('QvsD_', sys, '.csv' ,sep=''))
 }
 
-png("QvsD.png")
-plot(QList, type='o')
+write.csv(outdata, file=paste("outdata_", FolderName, ".csv", sep=''))
+
+png(paste("SRmap_", FolderName, ".png", sep=''))
+p1 <- levelplot(outdata[,3]~outdata[,2]*outdata[,1], asp=1, col.regions=col.l)
+print(p1)
 dev.off()
-
-
-
--- project/StochasticResonance/preprocess.r
+++ project/StochasticResonance/preprocess.r
@@ -1,31 +1,55 @@
 require(jsonlite)
 
-sys <- "sys1_mc"
+##sys <- "ll_sys625_lg"
+##sys <- "sys_n_625_beta_0.5"
+sys <- "sys_n_2500_beta_0.5"
 js <- read_json(paste(sys, '.json', sep=''))
-Cfile <- file("commands", "w")
 
-id <- 0
-for ( temperature in seq(0.5, 20, by=0.5) )
+C2file <- file("total_commands", "w")
+
+case_id <- 0
+
+##A <- 0.7
+cc <- 0.75
+
+for ( A in seq(0.0, 0.9, by=0.05) )
 {
-    id = id + 1
-    cat("\n ***********run********* ", id, "\n")
-    out <- paste(sys, '_', id, '.json', sep='')
-    FolderName <- paste("case_", sys, "_", as.character(id), sep='')
-    dir.create(FolderName)
+    case_id <- case_id + 1
+    CfileName <- paste("commands_", as.character(case_id), sep='')
+    Cfile <- file(CfileName, "w")
+    id <- 0
+    for ( temperature in seq(0, 0.6, by=0.03) )
+        ##for ( temperature in seq(10, 200, by=10) )
+        ##for ( AE in seq(0.1, 2, by=0.1) )
+    {
+        id = id + 1
+        cat("\n ***********run********* ", id, "\n")
+        out <- paste(sys, '_', id, '.json', sep='')
+        FolderName <- paste("case_", cc, "_", sys, "_", as.character(case_id), "_", as.character(id), sep='')
+        dir.create(FolderName)
 
-    js$worlds[[1]]$temperature <- temperature
-    ## Copy the seqfile.txt into folder
-    write_json(js, paste(FolderName, '/', out, sep=''), pretty = TRUE, auto_unbox = TRUE)
-    cmd4 <- paste("cp ../../build/saphron", FolderName)
-    system(cmd4)
+        js$worlds[[1]]$temperature <- temperature
+        js$forcefields$A <- A
+        js$forcefields$constraints[[1]]$coefficient = cc
+        ##js$forcefields$constraints[[1]]$coefficient <- AE
+        ## Copy the seqfile.txt into folder
+        write_json(js, paste(FolderName, '/', out, sep=''), pretty = TRUE, auto_unbox = TRUE)
+        cmd4 <- paste("cp ../../build/saphron", FolderName)
+        system(cmd4)
 
-    runcmd <- paste("cd", FolderName, "; screen -d -m ../../../build/saphron ", out, "; cd ..")
-    ##cat(paste("sleep ", rtime, "; cd", FolderName, "; PhysModel; cd .."), file="run")
-    ##runcmd <- paste("screen -d -m ", FolderName, "/PhysModel", sep='')
-    ##runcmd <- paste("bsub ", FolderName, "/run -o output_", i, sep='')	
-    ##runcmd <- paste("cd", FolderName, "; pwd; BigCAT; cd ..;")
-    writeLines(runcmd, Cfile)
+        ##runcmd <- paste("cd", FolderName, "; screen -d -m ../../../build/saphron ", out, "; cd ..")
+        runcmd <- paste("cd", FolderName, "; ../../../build/saphron ", out, "; cd ..;")
+        ##cat(paste("sleep ", rtime, "; cd", FolderName, "; PhysModel; cd .."), file="run")
+        ##runcmd <- paste("screen -d -m ", FolderName, "/PhysModel", sep='')
+        ##runcmd <- paste("bsub ", FolderName, "/run -o output_", i, sep='')	
+        ##runcmd <- paste("cd", FolderName, "; pwd; BigCAT; cd ..;")
+        writeLines(runcmd, Cfile)
+    }
+    close(Cfile)
+    cmd <- paste("chmod +x", CfileName)
+    system(cmd)
 
+    runcmd <- paste("screen -d -m", CfileName)
+    writeLines(runcmd, C2file)
 }
-close(Cfile)
-
+close(C2file)
